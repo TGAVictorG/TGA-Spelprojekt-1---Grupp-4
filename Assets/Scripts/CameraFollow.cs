5e886564d7a52a78325a4f5d44f1b6e8ed338e11
Sparkbrain <victorgranberg94@hotmail.com>
2020-09-22 17:54:31 +0200

    First draft plane movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform myTarget;
+    [SerializeField] Vector3 myTargetOffset;
+    [SerializeField] bool myUseWorldOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPosition();
+        SetRotation();
+    }
+
+    void SetPosition()
+    {
+        Vector3 targetPosition = myTarget.position;
+
+        if(!myUseWorldOffset)
+        {
+            transform.position = targetPosition + myTargetOffset;
+        }
+        else
+        {
+            transform.position = myTarget.TransformPoint(myTargetOffset);
+        }
+    }
+
+    void SetRotation()
+    {
+        transform.LookAt(myTarget);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float myBaseMovementSpeed = 1;
+
+
+    [SerializeField] private float myPitchMultiplier = 1;
+    [SerializeField] private bool myPitchInvertState = false;
+    [SerializeField] private float myRollModifier = 1;
+    [SerializeField] private bool myRollInvertState = false;
+    [SerializeField] private float myYawModifier = 1;
+    [SerializeField] private bool myYawInvertState = false;
+
+    private Rigidbody myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.velocity = transform.forward * myBaseMovementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        AddTorque();
+        myRigidbody.velocity = transform.forward * myBaseMovementSpeed;
+    }
+
+    private void AddTorque()
+    {
+        AddPitch();
+        AddRoll();
+        AddYaw();
+    }
+
+    private void AddPitch()
+    {
+        float pitchValue = Input.GetAxis("Pitch");
+        int invertValue = myPitchInvertState ? -1 : 1;
+        transform.Rotate(transform.right, pitchValue * myPitchMultiplier * invertValue, Space.World);
+    }
+
+    private void AddRoll()
+    {
+        float rollValue = Input.GetAxis("Roll");
+        int invertValue = myRollInvertState ? -1 : 1;
+        transform.Rotate(transform.forward, rollValue * myRollModifier * invertValue, Space.World);
+    }
+
+    private void AddYaw()
+    {
+        float yawValue = Input.GetAxis("Yaw");
+        int invertValue = myYawInvertState ? -1 : 1;
+        transform.Rotate(transform.up, yawValue * myYawModifier * invertValue, Space.World);
+    }
+}
