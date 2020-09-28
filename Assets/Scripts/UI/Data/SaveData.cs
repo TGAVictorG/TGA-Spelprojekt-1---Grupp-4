e6f72216d5a5b2564c9941f9b206f6f9cef798bf
Zaid Neurothrone <neurothrone@outlook.com>
2020-09-28 09:46:54 +0200

    Import and couple OptionsMenu to Main Menu

---
+++ Assets/Scripts/UI/Data/SaveData.cs
@@ -0,0 +1,37 @@
+﻿namespace UI.Data
+{
+    //-------------------------------------------------------------------------
+    [System.Serializable]
+    public class SaveData
+    {
+        // Audio Data
+        public float myMasterVolume;
+        public float myMusicVolume;
+        public float mySFXVolume;
+        public float myVoiceVolume;
+        
+        // Video Data
+        public bool myIsFullScreenOn;
+        public bool myIsVsyncOn;
+        public Resolution myResolution = null;
+
+        //-------------------------------------------------
+        public SaveData()
+        {
+            SetDefaultAudioSettings();
+
+            myIsFullScreenOn = false;
+            myIsVsyncOn = false;
+            myResolution = new Resolution {myWidth = 1920, myHeight = 1080};
+        }
+
+        //-------------------------------------------------
+        public void SetDefaultAudioSettings()
+        {
+            myMasterVolume = 0.0f;
+            myMusicVolume = 0.0f;
+            mySFXVolume = 0.0f;
+            myVoiceVolume = 0.0f;
+        }
+    }
+}
