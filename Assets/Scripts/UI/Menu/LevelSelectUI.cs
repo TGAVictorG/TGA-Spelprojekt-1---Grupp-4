66b173b9949977935f179862c522824d95a03668
OliwerDahlinTGA <71003274+OliwerDahlinTGA@users.noreply.github.com>
2020-09-28 16:33:01 +0200

    Level select + beginning og level unlock system

---
+++ Assets/Scripts/UI/Menu/LevelSelectUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectUI : MonoBehaviour
+{
+
+    public Image[] myStageUnlockGraphics;
+
+    GameManager myGameManager = GameManager.ourInstance;
+
+    public void LoadLevel(int aSceneBuildIndex)
+    {
+        myGameManager.TransitionToStage(aSceneBuildIndex);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < myStageUnlockGraphics.Length; i++)
+        {
+            if(myGameManager.IsStagedUnlocked(i) == true)
+            {
+               // myStageUnlockGraphics[i].
+            }
+        }
+    }
+
+
+}
