0561b1d275a04e269ddb6ce4b1a9eee415a4d884
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-09-29 17:11:22 +0200

    Improvements for stage handling + loading & saving

---
+++ Assets/Scripts/Data/StageInformation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stage Information", menuName = "Stage/Data/Stage Information")]
+public class StageInformation : ScriptableObject
+{
+    public string myStageDisplayName;
+    public string myStageSceneName;
+
+    public bool myIsForceUnlocked;
+
+    public Sprite myStageThumbnail;
+}
