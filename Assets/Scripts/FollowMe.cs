8f7bef00227e6d0218b502b7a58353dda5de6f09
SerialCoderLuffy <oldulf@hotmail.com>
2020-10-05 16:38:15 +0200

    Adding Train prefab alpha. Contains Route-information. Uses Route, RouteFollow and FollowMe Scripts.

---
+++ Assets/Scripts/FollowMe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMe : MonoBehaviour
+{
+    [SerializeField] private Transform myFollower;
+    private Queue<Vector3> myPreviousPositions;
+    private float myTimeCounter = 0f;
+    [SerializeField] private float myFollowerTimeOffset = 0.65f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPreviousPositions = new Queue<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > 0)
+        {
+            myTimeCounter += Time.deltaTime;
+            myPreviousPositions.Enqueue(transform.position);
+            if (myTimeCounter > myFollowerTimeOffset)
+            {
+                myFollower.position = myPreviousPositions.Dequeue();
+                myFollower.LookAt(myPreviousPositions.Peek());
+            }
+            
+        }
+    }
+}
