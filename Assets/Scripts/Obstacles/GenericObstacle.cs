2e8502fd8acea6c2ae87ce670b6440195bd3f482
Hapeing <hampushallstrom@hotmail.com>
2020-10-15 19:32:53 +0200

    death animation is now played

--- Assets/Scripts/Obstacles/GenericObstacle.cs
+++ Assets/Scripts/Obstacles/GenericObstacle.cs
@@ -3,11 +3,23 @@
 public class GenericObstacle : MonoBehaviour
 {
 
+    [SerializeField] private bool myStunnedOnImpact = true;
+    [SerializeField] private float myTimeBeforeDeathScreen = 3;
+    private float mydeadTime;
+    private void Awake()
+    {
+        mydeadTime = myTimeBeforeDeathScreen;
+    }
+
     public void OnCollisionEnter(Collision aCollision)
     {
+
+
+
         Debug.Assert(aCollision.contactCount > 0, "Collision without contact points!");
 
         ContactPoint contactPoint = aCollision.GetContact(0);
+
 
 
 
@@ -19,7 +31,35 @@ public class GenericObstacle : MonoBehaviour
 
         if (angle < 45.0f)
         {
+
+            GetComponentInChildren<Animator>().Play("Dead");
+            mydeadTime -= Time.deltaTime;
+            if (myStunnedOnImpact)
+            {
+
+                GetComponent<PlaneController>().enabled = false;
+            }
+        }
+
+
+    }
+
+    private void Update()
+    {
+        if (mydeadTime < myTimeBeforeDeathScreen)
+        {
+            mydeadTime -= Time.deltaTime;
+        }
+        if (mydeadTime < 0)
+        {
+
             UI.EndScreenMenu.ourInstance.DisplayEndScreen(false);
+            mydeadTime = myTimeBeforeDeathScreen;
+            if (myStunnedOnImpact)
+            {
+                GetComponent<PlaneController>().enabled = true;
+            }
         }
     }
+
 }
