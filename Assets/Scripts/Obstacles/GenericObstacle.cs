97ff3098779cb9379a74078d2f29c58978bda58c
Oliwer Dahlin <oliwerdahlin@gmail.com>
2020-10-15 17:40:46 +0200

    CameraShakeImplemenatation p1

--- Assets/Scripts/Obstacles/GenericObstacle.cs
+++ Assets/Scripts/Obstacles/GenericObstacle.cs
@@ -2,6 +2,14 @@
 
 public class GenericObstacle : MonoBehaviour
 {
+
+    ShakeableTransform myCameraShake;
+
+    private void Awake()
+    {
+        myCameraShake = Camera.main.GetComponent<ShakeableTransform>();
+    }
+
     public void OnCollisionEnter(Collision aCollision)
     {
         Debug.Assert(aCollision.contactCount > 0, "Collision without contact points!");
@@ -12,6 +20,7 @@ public class GenericObstacle : MonoBehaviour
         if (angle < 45.0f)
         {
             // TODO: Kill player
+            myCameraShake.ShakeCamera(1);
         }
     }
 }
--- Assets/Scripts/ShakeableTransform.cs
+++ Assets/Scripts/ShakeableTransform.cs
@@ -6,15 +6,13 @@ public class ShakeableTransform : MonoBehaviour
 {
 
     public float myFrequency = 10f;
-    public float myTrauma;
+    private float myTrauma;
     public float myRecoverySpeed = 1.5f;
 
-    public bool myIsDebugging = false;
 
     float myTraumaExponent = 2;
 
-   // public Vector3 myMaximumTranslationShake = Vector3.one * 0.5f;
-    public Vector3 myMaximumAngularShake = Vector3.one * 2;
+    public Vector3 myMaximumAngularShake = Vector3.one;
 
     private float mySeed;
 
@@ -28,17 +26,16 @@ public class ShakeableTransform : MonoBehaviour
     void Update()
     {
 
-        if(Input.GetMouseButtonDown(0) && myIsDebugging)
+        if(myTrauma != 0)
         {
-            ShakeCamera(1);
+
+            float shake = Mathf.Pow(myTrauma, myTraumaExponent);
+
+            transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.x + (myMaximumAngularShake.x * ((Mathf.PerlinNoise(mySeed + 3, Time.time * myFrequency)) * 2) - 1), transform.localRotation.y + myMaximumAngularShake.y * (Mathf.PerlinNoise(mySeed + 4, Time.time * myFrequency) * 2 - 1), transform.localRotation.z + myMaximumAngularShake.z * (Mathf.PerlinNoise(mySeed + 5, Time.time * myFrequency) * 2 - 1)) * shake);
+
+            myTrauma = Mathf.Clamp01(myTrauma - myRecoverySpeed * Time.deltaTime);
         }
 
-
-        float shake = Mathf.Pow(myTrauma, myTraumaExponent);
-
-        transform.localRotation = Quaternion.Euler(new Vector3(myMaximumAngularShake.x * (Mathf.PerlinNoise(mySeed + 3, Time.time * myFrequency) * 2 - 1),myMaximumAngularShake.y * (Mathf.PerlinNoise(mySeed + 4, Time.time * myFrequency) * 2 - 1),myMaximumAngularShake.z * (Mathf.PerlinNoise(mySeed + 5, Time.time * myFrequency) * 2 - 1)) * shake);
-
-        myTrauma = Mathf.Clamp01(myTrauma - myRecoverySpeed * Time.deltaTime);
     }
 
     public void ShakeCamera(float aShakeAmount)
