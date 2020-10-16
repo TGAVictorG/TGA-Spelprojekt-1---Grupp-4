2edd56bc8479161acb0431e84a9e0a5ac7b3cd22
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-10-16 14:04:48 +0200

    Work on Player UI. Added letters and timer.

---
+++ Assets/Scripts/UI/Player/TimerUI.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI myTimerText;
+
+    private int myPrevDisplayTime = -1;
+
+    void Update()
+    {
+        if (Mathf.FloorToInt(Time.time) != myPrevDisplayTime)
+        {
+            myPrevDisplayTime = Mathf.FloorToInt(Time.time);
+
+            float duration = Time.time - StageManager.ourInstance.myStageStartTime;
+
+            int minutes = Mathf.FloorToInt(duration / 60.0f);
+            int seconds = Mathf.FloorToInt(duration) % 60;
+
+            myTimerText.text = $"{minutes:D2}:{seconds:D2}";
+        }
+    }
+}
