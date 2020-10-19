16a6453a760dab8b8a9085343d0a775c31fd153c
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-10-19 16:02:50 +0200

    Better player death handling + killed by discolaser

---
+++ Assets/Scripts/PlayerDeathHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerDeathHandler : MonoBehaviour
+{
+    public enum DeathReason
+    {
+        Collision,
+        Laser
+    }
+
+    private PlaneController myPlaneController;
+    private Rigidbody myRigidbody;
+    private Animator myAnimator;
+
+    private ShakeableTransform myCameraShake;
+
+    public void Kill(DeathReason aDeathReason, float aDelay, bool aStunPlayer = true)
+    {
+        if (StageManager.ourInstance.myIsPlayerDead)
+        {
+            return;
+        }
+
+        StageManager.ourInstance.OnPlayerDied();
+
+        if (aStunPlayer)
+        {
+            myPlaneController.enabled = false;
+            myRigidbody.useGravity = true;
+
+            myCameraShake.ShakeCamera();
+        }
+
+        if (aDeathReason == DeathReason.Collision)
+        {
+            myAnimator.Play("Dead");
+        }
+
+        StartCoroutine(DelayedDeath(aDelay));
+    }
+
+    private IEnumerator DelayedDeath(float aDelay)
+    {
+        yield return new WaitForSeconds(aDelay);
+
+        UI.EndScreenMenu.ourInstance.DisplayEndScreen(false);
+    }
+
+    private void Awake()
+    {
+        myPlaneController = GetComponent<PlaneController>();
+        myRigidbody = GetComponent<Rigidbody>();
+        myAnimator = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        myCameraShake = Camera.main.GetComponent<ShakeableTransform>();
+    }
+}
