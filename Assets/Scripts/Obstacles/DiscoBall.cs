9d96d488a42b32559c25569950f6493a78fd7707
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-10-20 10:08:38 +0200

    Improve laser, change laser length to avoid clipping colliders

--- Assets/Scripts/Obstacles/DiscoBall.cs
+++ Assets/Scripts/Obstacles/DiscoBall.cs
@@ -8,7 +8,17 @@ public class DiscoBall : MonoBehaviour
     [SerializeField]
     private float myLossScreenDelay = 2.0f;
 
+    [SerializeField]
+    private float myLaserRadius = 1.2f;
+
+    [SerializeField]
+    private float myOvershoot = 0.2f;
+
+    [SerializeField]
+    private Transform[] myLaserTransforms;
+
     private Rigidbody myRigidbody;
+    private int myLayerMask;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,13 +36,40 @@ public class DiscoBall : MonoBehaviour
         }
     }
 
+    private void UpdateLaserScale()
+    {
+        Vector3 globalScale = transform.lossyScale;
+        for (int i = 0; i < myLaserTransforms.Length; ++i)
+        {
+            Transform laserTransform = myLaserTransforms[i];
+
+            float length = 50.0f;
+
+            Vector3 origin = laserTransform.position - laserTransform.forward;
+            Ray ray = new Ray(origin, laserTransform.forward);
+
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, myLayerMask))
+            {
+                length = (hitInfo.distance - 1.0f + myOvershoot) / (2.0f * globalScale.x);
+            }
+
+            laserTransform.localScale = new Vector3(myLaserRadius, myLaserRadius, length);
+        }
+    }
+
     private void FixedUpdate()
     {
         myRigidbody.rotation *= Quaternion.AngleAxis(myRotationSpeed * Time.deltaTime, Vector3.up);
+        UpdateLaserScale();
     }
 
     private void Awake()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        myLayerMask = ~(1 << LayerMask.NameToLayer("Ignore Raycast"));
+
+        UpdateLaserScale();
     }
 }
