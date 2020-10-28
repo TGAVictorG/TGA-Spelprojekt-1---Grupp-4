13e705538de43449805eeb066483caab3e9d9092
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-10-28 16:38:13 +0100

    Quick revert

--- Assets/Scripts/UI/Menu/MainMenuCamera.cs
+++ Assets/Scripts/UI/Menu/MainMenuCamera.cs
@@ -1,13 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuCamera : MonoBehaviour
 {
+    private static bool myHasStarted = false;
+
+    // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(10, LoadSceneMode.Additive);
+        if (myHasStarted == false)
+        {
+            myHasStarted = true;
+            SceneManager.LoadScene(10, LoadSceneMode.Additive);
+        }
     }
 
+    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up, Time.deltaTime * 5);
