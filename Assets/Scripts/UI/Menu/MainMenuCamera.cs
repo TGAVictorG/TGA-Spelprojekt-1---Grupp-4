68e16f45e677f89859082fc2b89d0d9e390d4190
Hapeing <hampushallstrom@hotmail.com>
2020-10-28 13:53:45 +0100

    new main menu

---
+++ Assets/Scripts/UI/Menu/MainMenuCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(10, LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 5);
+    }
+}
