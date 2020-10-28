d3f0ebda19b0624c6027928aa64f3e00c91a177f
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-10-28 16:43:59 +0100

    Fix MainMenu bg scene not loading after the first time

--- Assets/Scripts/UI/Menu/MainMenuCamera.cs
+++ Assets/Scripts/UI/Menu/MainMenuCamera.cs
@@ -1,20 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuCamera : MonoBehaviour
 {
-    private static bool myHasStarted = false;
-
-    // Start is called before the first frame update
     void Start()
     {
-        if (myHasStarted == false)
-        {
-            myHasStarted = true;
-            SceneManager.LoadScene(10, LoadSceneMode.Additive);
-        }
+        SceneManager.LoadScene(10, LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
